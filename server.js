897f69dff66ab8429badbd85d0f6afe2ea872c04
MeganusWesker <mohitjassal157@gmail.com>
2023-03-27 15:52:58 +0530

    new

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ config({path:"config.env"});
 
 module.exports.io=new Server(server,{
   cors:{
-      origin:'*',
-      
+      origin:[process.env.FRONTEND_URL],
+      credentials:true,
   }
 });
 
